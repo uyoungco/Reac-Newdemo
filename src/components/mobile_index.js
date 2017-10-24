d2f33dbc91cd72c025209ba645a9e09e55461d90
uyoung <zzyouyang@qq.com>
2017-10-25 01:03:33 +0800

    移动端导航设计

---
+++ src/components/mobile_index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MobileHeder from './mobile_header';
+import '../css/mobile.css';
+
+export default class MobileIndex  extends React.Component {
+  render() {
+    return(
+      <div>
+        <MobileHeder />
+      </div>
+    );
+  };
+}
--- src/root.js
+++ src/root.js
@@ -2,18 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MediaQuery from 'react-responsive';
 
+import 'antd/dist/antd.css';
 import { Button } from 'antd';
 import PCIndex from './components/pc_index';
-import 'antd/dist/antd.css';
+import MobileIndex from './components/mobile_index';
 
 class Root extends React.Component {
   render() {
     return (
-
       <div>
-        <PCIndex />
-        
+        <MediaQuery query="(min-device-width: 1224px)">
+          <PCIndex />
+        </MediaQuery>
+        <MediaQuery query="(max-device-width: 1224px)">
+          <MobileIndex />
+        </MediaQuery>
       </div>
     );
   }
