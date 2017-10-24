ef53bab20b292c0b0b37e9cc5ba22b80898bda5a
uyoung <zzyouyang@qq.com>
2017-10-25 01:21:38 +0800

    移动和PC的页脚设计

--- src/components/pc_index.js
+++ src/components/pc_index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import PCHeader from './pc_header';
+import PCFooter from './pc_footer';
 import '../css/pc.css';
 
 
@@ -9,6 +10,7 @@ export default class PCIndex extends React.Component {
     return(
       <div>
         <PCHeader />
+        <PCFooter />
       </div>
     );
   };
